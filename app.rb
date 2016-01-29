d0e459284acca28b31ad702045457241eae744a4
kyle <altck8@gmail.com>
2016-01-29 10:32:24 -0800

    add user ability to add shoes and stores to db

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/reloader'
 require 'pry'
+require './lib/shoe'
+require './lib/store'
 also_reload( './lib/**/*.rb')
 
 get '/' do
+  @shoes = Shoe.all
+  @stores = Store.all
   erb :index
+end
+
+post '/add_shoe' do
+  Shoe.create( {name: params.fetch("shoe_name"), cost: params.fetch("shoe_cost")} )
+  redirect '/'
+end
+
+post '/add_store' do
+  Store.create( {name: params.fetch("store_name"), zipcode: params.fetch("store_zip")} )
+  redirect '/'
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,4 @@
 
-class ShoeStore < ActiveRecord::Base
+class Store < ActiveRecord::Base
   has_and_belongs_to_many :shoes
 end
